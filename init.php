8468d232bd9626fa128f0db5e1af1daa709026ac
Shah jalal <rjshahjalal132@gmail.com>
2023-12-30 12:38:42 +0600

    added a languages directory and removed uninstall.php file

---
+++ init.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Plugin Name:       oop-plugin-structure
+ * Plugin URI:        #
+ * Description:       A brief description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shah jalal
+ * Author URI:        #
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       oop-plugin-structure
+ * Domain Path:       /languages
+ */
+
+?>
